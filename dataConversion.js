b19524fa94322af0912e28321df17e60cf331978
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-15 09:13:11 +0800

    Initial commit

---
+++ dataConversion.js
@@ -0,0 +1,17 @@
+// Convert a String into a Number
+let price = "200";
+price = parseInt(price);
+console.log(price);
+console.log(typeof price);
+
+// Convert a Number to a String
+let distance = "150";
+distance = distance.toString();
+console.log(distance);
+console.log(typeof distance);
+
+// Convert a String into a Decimal Number
+let weight = "72.5";
+weight = parseFloat(weight);
+console.log(weight);
+console.log(typeof weight);
