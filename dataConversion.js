8d80461d865b3f6a81e95dfd868c06cab29da817
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-15 16:11:52 +0800

    Initial commit

--- dataConversion.js
+++ dataConversion.js
@@ -1,12 +1,26 @@
 // Convert a String into a Number
 let price = "200";
-price = parseInt(price);
+
+price = parseInt(price); // Using parseInt() method
+console.log(price);
+console.log(typeof price);
+
+price = Number(price) // Using Number() method
+console.log(price)
+console.log(typeof price);
+
+price = + (price); // Using + Operator method
 console.log(price);
 console.log(typeof price);
 
 // Convert a Number to a String
 let distance = "150";
-distance = distance.toString();
+
+distance = distance.toString(); // Using toString() method
+console.log(distance);
+console.log(typeof distance);
+
+distance = String(distance); // Using String() method
 console.log(distance);
 console.log(typeof distance);
 
